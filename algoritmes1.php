f42e963fd356fbea43de31a209cbc4e94492f844
AdrigomRoc <adrigomroc@gmail.com>
2018-07-06 09:37:43 +0200

    Funcio mult

--- algoritmes1.php
+++ algoritmes1.php
@@ -12,7 +12,17 @@
                 return $res;
             }
         }
-        echo "El resultat es ".suma(5,6);
+        function mult($var1,$var2){
+            if(!is_numeric($var1) || !is_numeric($var2)){
+                return "erroni";
+            }else{
+                $res = "$var1 * $var2 = ".$var1 * $var2;
+                return $res;
+            } 
+        }
+        
+        echo "El resultat es ".suma(5,6)."<br>";
+        echo mult(4,5)."<br>"
     ?>
     
    
