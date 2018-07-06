a70ce20bfbafea8a781df1780ed924367ccb4a83
AdrigomRoc <adrigomroc@gmail.com>
2018-07-06 09:17:29 +0200

    Funcio suma
    Detecta els strings i si no es String fa el calcul

--- algoritmes1.php
+++ algoritmes1.php
@@ -4,7 +4,14 @@
 <body>
 
     <?php
-    
+        function($var1,$var2){
+            if(!is_numeric($var1) || !is_numeric($var2)){
+                echo "es un string";
+            }else{
+                $res = $var1 + $var2;
+                return $res;
+            }
+        }
     ?>
 
 </body>
