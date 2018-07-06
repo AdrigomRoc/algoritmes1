fe478ac0bfe0adaa4b7230acf814f456bd79ec55
AdrigomRoc <adrigomroc@gmail.com>
2018-07-06 09:26:01 +0200

    pulir funcio

--- algoritmes1.php
+++ algoritmes1.php
@@ -4,16 +4,18 @@
 <body>
 
     <?php
-        function($var1,$var2){
+        function suma($var1,$var2){
             if(!is_numeric($var1) || !is_numeric($var2)){
-                echo "es un string";
+                return "erroni";
             }else{
                 $res = $var1 + $var2;
                 return $res;
             }
         }
+        echo "El resultat es ".suma(5,6);
     ?>
-
+    
+   
 </body>
 
 </html>
